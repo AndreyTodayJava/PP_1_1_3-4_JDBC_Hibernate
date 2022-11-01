2aa986d3187a99127945da9f742c275ea5962377
AndreyTodayJava <andrey258555@gmail.com>
2022-11-01 22:06:44 +0300

    1try

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,15 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,31 +17,95 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("create table if not exists users (id int auto_increment, name varchar(45), " +
+                    "lastName varchar(45), age int, primary key (id))").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            System.err.println("Ошибка при создании таблицы");
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void dropUsersTable() {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createSQLQuery("drop  table if exists users").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+        if(transaction != null) {
+            transaction.rollback();
+        }
+        System.err.println("Ошибка при удалении таблицы");
+        e.printStackTrace();
+    }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        User user = new User(name, lastName, age);
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
 
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+
+            System.out.println("User с именем -" + name + " " + lastName + " добавлен в базу данных");
+        } catch (HibernateException e) {
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            System.err.println("Ошибка при добавлении пользователя");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction transaction = null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            System.err.println("Ошибка при удалении пользователя");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.getSessionFactory().openSession()) {
+          return session.createQuery("from User", User.class).list();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Transaction transaction = null;
+        try(Session session = Util.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.createQuery("delete User").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            System.err.println("Ошибка при очистке таблицы");
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,35 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
 
+    UserDao userDaoHiberbate = new UserDaoHibernateImpl();
+    public void createUsersTable() {
+        userDaoHiberbate.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        userDaoHiberbate.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        userDaoHiberbate.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        userDaoHiberbate.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return userDaoHiberbate.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+        userDaoHiberbate.cleanUsersTable();
     }
 }
